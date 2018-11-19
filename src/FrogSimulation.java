16393ef454c980b2e523d7ffc6c7baafbfca06be
a <a>
2018-11-19 10:01:43 -0500

    asd

--- src/FrogSimulation.java
+++ src/FrogSimulation.java
@@ -12,15 +12,30 @@ public class FrogSimulation {
         }
 
         private int hopDistance(){
-            
+
+            int num = (int)((Math.random()*100)-5);
+            int prob = (int)((Math.random()*100)-1);
+            if (prob > 50)
+            {
+
+                return num;
+            }
+            else
+            {
+                return -1*num;
+            }
 
         }
+       
         public boolean simulate()
         {
             int position = 0;
             for(int count = 0; count < maxHops; count++)
             {
-                position += hopDistance();
+                int array1 = new array1[maxHops];
+                int numb = hopDistance();
+                position += numb;
+                array1[count]=numb;
                 if(position >= goalDistance)
                 {
                     return true;
@@ -34,13 +49,13 @@ public class FrogSimulation {
 
         public double runSimulation(int num)
         {
-            int countSucess=0;
+            int countSuccess=0;
             for(int i=0; i< num; i++)
             {
                 if(simulate()) {
-                    countSucess++;
+                    countSuccess++;
                 }
             }
-            return (double) countSucess/num;
+            return (double) countSuccess/num;
         }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -2,9 +2,15 @@ public class Runner {
 
     public static void main(String[] args)
     {
-        
+        FrogSimulation frog1 = new FrogSimulation(30, 5);
 
+        FrogSimulation frog2 = new FrogSimulation(50, 10);
 
+        FrogSimulation frog3 = new FrogSimulation(15, 2);
+
+        System.out.println("The Frog Success: " + frog1.runSimulation(5));
+        System.out.println("The Frog Success: " + frog2.runSimulation(5));
+        System.out.println("The Frog Success: " + frog3.runSimulation(5));
 
     }
 
